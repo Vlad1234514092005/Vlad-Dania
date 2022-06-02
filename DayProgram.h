aca2abb0f8c6bd271d666dd125ca3128399c2d19
VladTerebylo <terebilo.vlad1409@gmail.com>
2022-06-02 17:45:50 +0200

    DayProgram

---
+++ DayProgram.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+#include "Teleprogram.h"
+#include"Time_.h"
+#include"Date.h"
+#include "Teleprogram.h"
+class DayProgram
+{
+};
+
